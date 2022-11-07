fe668c959e1b2ff4b8fb698b0b06d9b9a30255b8
Từ Phước Trung <whillits@gmail.com>
2022-11-07 10:28:47 +0700

    update index.php thêm lại script tinymce

--- index.php
+++ index.php
@@ -46,12 +46,12 @@ spl_autoload_register();
 <body>
   <h1>Xin chòa</h1>
   <div id="maintest-nav-oragne">
-    <?php 
-      include "View/dautrang.php";
+    <?php
+    include "View/dautrang.php";
     ?>
 
     <?php
-      include "View/banner.php";
+    include "View/banner.php";
     ?>
 
     <div class="container-fluid">
@@ -71,7 +71,41 @@ spl_autoload_register();
     ?>
   </div>
   <!-- <script src="https://cdn.tiny.cloud/1/5to837jgaayhbjkam58rzcyye5p6nut0nzdktthsp7bpuj5q/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script> -->
-  <script src="./View/ajax/tinymce.js"></script>
+  <script>
+    $(document).ready(function() {
+      tinymce.init({
+        selector: '#noidungbaiviet',
+        height: 200,
+        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect',
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        tinycomments_mode: 'embedded',
+        tinycomments_author: 'Author name',
+        mergetags_list: [{
+            value: 'First.Name',
+            title: 'First Name'
+          },
+          {
+            value: 'Email',
+            title: 'Email'
+          },
+        ]
+      });
+      // tinymce.init({
+      //   selector: '#noidungbaiviet',
+      //   plugins: [
+      //     'a11ychecker', 'advlist', 'advcode', 'advtable', 'autolink', 'checklist', 'export',
+      //     'lists', 'link', 'image', 'charmap', 'preview', 'anchor', 'searchreplace', 'visualblocks',
+      //     'powerpaste', 'fullscreen', 'formatpainter', 'insertdatetime', 'media', 'table', 'help', 'wordcount'
+      //   ],
+      //   toolbar: 'undo redo | a11ycheck casechange blocks | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist checklist outdent indent | removeformat | code table help'
+
+      // });
+
+
+
+
+    })
+  </script>
   <!-- <script type="text/javascript">
     var filterValue = document.querySelectorAll('input[name="filter"]');
 
